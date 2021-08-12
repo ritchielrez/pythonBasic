1d633b3242d101723a27917eb1b03ff17ec2b9a7
RitchielRez <you@example.com>
2021-08-12 22:02:34 +0600

    Removed useless scripts

--- dimbrightness.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def dimbrightness():
-    os.system('xbacklight -get > prevbrightness.txt')
-
-    os.system('xbacklight -set 0')        
-
--- undimbrightness.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def undimbrightness():
-    with open('currentbrightness.txt', 'r') as prevbrightness:
-        prevbrightness = prevbrightness.read()
-
-        os.system(f'xbacklight -set {prevbrightness}')
-
