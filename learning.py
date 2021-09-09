994dbf1c246f5650e957cce75f720f23362dfbfd
RitchielRez <ritchielrez@gmail.com>
2021-09-09 12:35:53 +0600

    Solved problem of find out a number has a square root or not

--- learning.py
+++ learning.py
@@ -1,6 +1,38 @@
-import os
-import sys
-import random
+import math as Math
+
+def isFloat(num, index):
+    while index != len(num):
+        if(num[index] == '0'):
+            index = index + 1
+        else:
+            return False
+
+    return True
 
 if __name__ == '__main__':
-    print('Hello World')
+    T = int(input())
+
+    for i in range(T):
+        N = float(input())
+
+        n = str(Math.sqrt(N))
+
+        index = n.find('.') + 1
+
+        if isFloat(n, index):
+            print('YES')
+        else:
+            print('NO')
+
+        
+
+            
+
+
+        
+
+        
+        
+
+        
+
